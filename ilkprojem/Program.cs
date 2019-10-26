28fb455a9fb797cc5c69cb79aeaa076ffd6fb642
mguntay <guntay11@gmail.com>
2019-10-26 12:14:37 +0300

    Add project files.

---
+++ ilkprojem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ilkprojem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen kendinizi tanıtınız...");
+            Console.WriteLine("Giriş yapan kullanıcı:" + Console.ReadLine());
+
+        }
+    }
+}
